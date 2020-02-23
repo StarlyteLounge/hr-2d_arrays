c7c12c813aaa21302d2aed6cef76f90d6aa6ae16
Jon McCarty <jdmc020@gmail.com>
2020-02-23 15:43:21 -0500

    Return largest sum of any single hour glass. Added test data.

--- main.py
+++ main.py
@@ -11,17 +11,14 @@ import sys
 if __name__ == '__main__':
     arr = []
     total = 0
+    max_sum = 0
 
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
     for r in range(4):  # hour glass must have 3 rows, so don't look at the last 2
-        for i in range(3):  # stop after the last 3-digits on a row
-            if arr[r][i] * arr[r][i+1] * arr[r][i+2]:  # are all non-zero?
-                # possible hourglass
-                if arr[r+1][i+1]:
-                    if arr[r+2][i] * arr[r+2][i+1] * arr[r+2][i+2]:
-                        # we have an hour glass, sum the components
-                        total = arr[r][i] + arr[r][i+1] + arr[r][i+2]
-                        total += arr[r+1][i+1]
-                        total += arr[r+2][i] + arr[r+2][i+1] + arr[r+2][i+2]
-    print(total)
+        for i in range(4):  # stop after the last 3-digits on a row
+            total = arr[r][i] + arr[r][i+1] + arr[r][i+2]
+            total += arr[r+1][i+1]
+            total += arr[r+2][i] + arr[r+2][i+1] + arr[r+2][i+2]
+            max_sum = max(max_sum, total)
+    print(max_sum)
